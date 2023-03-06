8f49eff0eb7515f546f04179afa069e38c38c5d4
3341 <40583390+3341@users.noreply.github.com>
2023-03-06 07:19:39 +0000

    test

---
+++ data.h
@@ -0,0 +1,10 @@
+export const int MAX_STROAGE_SIZE = 10086;
+
+struct Student {
+	char name[];
+	char id[]; //学号 
+	int hasManagerPermission; //是否有更改其他学生的权限 
+	int score; //分数 
+	char password[]; //登录密码 
+	
+};
