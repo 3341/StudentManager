cfd894cc8a31ecf3878b2a0fdf8a2d9d92e52bcf
3341 <40583390+3341@users.noreply.github.com>
2023-03-06 07:05:25 +0000

    Main

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    printf("H");
+
+}
+
+
+void Welcome()// 欢迎界面函数 
+{
+ system("title 程序设计课程设计"); //设置会话的窗口标题为“程序设计课程设计”
+ system("color F4");  //设置背景色为亮白色，字体颜色为红色
+ system("cls");	//清屏
+ printf("\n\n\t\t|----------------------------------------|\n");
+ printf("\t\t|----------------------------------------|\n");
+ printf("\t\t|     《程序设计》课程设计             |\n");
+ printf("\t\t|         ……………………………       |\n");
+ printf("\t\t|----------------------------------------|\n");
+ printf("\t\t|----------------------------------------|\n");
+ printf("\n\t\t		……");
+ printf("\n\t\t         ……\n");
+ printf("\n\t\t         ……\n");
+ printf("\n\t\t         ……\n");
+ printf("\n\t\t         ……\n"); 
+ printf("\n\t\t\t 	……\n");
+ 
+ }
+
+int login() {
+    
+}
+
+int controller() {
+
 }
